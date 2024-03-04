f6b95239144d178c7e53f999dff622b28d148154
Duy Nguyen <113152725+duytng93@users.noreply.github.com>
2024-03-04 15:33:50 -0600

    Update OpenAi.java

--- src/main/java/com/commercebank/OpenAi.java
+++ src/main/java/com/commercebank/OpenAi.java
@@ -8,12 +8,13 @@ import com.theokanning.openai.service.OpenAiService;
 
 @Service
 public class OpenAi {
-    private OpenAiService service = new OpenAiService("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");
+
+    private OpenAiService service = new OpenAiService(System.getenv("OPENAI_API_KEY"));
 
     public String generateText(String prompt) {
         CompletionRequest completionRequest = CompletionRequest.builder()
                 .prompt("Somebody once told me the world is gonna roll me")
-                .model("gpt-3.5-turbo-0125")
+                .model("babbage-002")
                         .echo(true)
                         .build();
 //        CompletionResponse completion = openAI.createCompletion(completionRequest);
